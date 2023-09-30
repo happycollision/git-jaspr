6f012c130b659c3f6c7bca2f8e0b54339c0febc0
Michael Sims <michael.sims@trillianthealth.com>
2023-09-30 10:33:15 -0500

    Override toString for some models to make logs easier to read

--- git-kspr/src/main/kotlin/sims/michael/gitkspr/Model.kt
+++ git-kspr/src/main/kotlin/sims/michael/gitkspr/Model.kt
@@ -17,6 +17,7 @@ data class GitHubInfo(val host: String, val owner: String, val name: String)
 
 data class Commit(val hash: String, val shortMessage: String, val fullMessage: String, val id: String?) {
     val remoteRefName: String get() = "$REMOTE_BRANCH_PREFIX$id"
+    override fun toString() = "Commit(hash='$hash', shortMessage='$shortMessage', id='$id')"
 }
 
 data class RefSpec(val localRef: String, val remoteRef: String)
@@ -30,6 +31,15 @@ data class PullRequest(
     val body: String,
     // TODO add state?
     // TODO add draft?
-)
+) {
+    override fun toString() =
+        "PullRequest(" +
+            "id='$id', " +
+            "commitId='$commitId', " +
+            "headRefName='$headRefName', " +
+            "baseRefName='$baseRefName', " +
+            "title='$title'" +
+            ")"
+}
 
 class GitKsprException(override val message: String) : RuntimeException(message)
