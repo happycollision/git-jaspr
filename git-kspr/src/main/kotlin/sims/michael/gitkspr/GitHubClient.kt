d33f232f8cfc043d60bff79a68a6576da9afe32a
Michael Sims <michael.sims@trillianthealth.com>
2023-09-30 15:45:43 -0500

    Update/create PR throw exceptions if the operation fails

--- git-kspr/src/main/kotlin/sims/michael/gitkspr/GitHubClient.kt
+++ git-kspr/src/main/kotlin/sims/michael/gitkspr/GitHubClient.kt
@@ -1,6 +1,7 @@
 package sims.michael.gitkspr
 
 import com.expediagroup.graphql.client.GraphQLClient
+import com.expediagroup.graphql.client.types.GraphQLClientResponse
 import org.slf4j.LoggerFactory
 import sims.michael.gitkspr.generated.CreatePullRequest
 import sims.michael.gitkspr.generated.GetPullRequests
@@ -36,36 +37,44 @@ class GitHubClient(private val delegate: GraphQLClient<*>, private val gitHubInf
     suspend fun createPullRequest(pullRequest: PullRequest) {
         logger.trace("createPullRequest {}", pullRequest)
         check(pullRequest.id == null) { "Cannot create $pullRequest which already exists" }
-        delegate.execute(
-            CreatePullRequest(
-                CreatePullRequest.Variables(
-                    CreatePullRequestInput(
-                        baseRefName = pullRequest.baseRefName,
-                        headRefName = pullRequest.headRefName,
-                        repositoryId = repositoryId(),
-                        title = pullRequest.title,
-                        body = pullRequest.body,
+        delegate
+            .execute(
+                CreatePullRequest(
+                    CreatePullRequest.Variables(
+                        CreatePullRequestInput(
+                            baseRefName = pullRequest.baseRefName,
+                            headRefName = pullRequest.headRefName,
+                            repositoryId = repositoryId(),
+                            title = pullRequest.title,
+                            body = pullRequest.body,
+                        ),
                     ),
                 ),
-            ),
-        )
+            )
+            .also { response ->
+                response.checkNoErrors { logger.error("Error creating PR {}", pullRequest) }
+            }
     }
 
     suspend fun updatePullRequest(pullRequest: PullRequest) {
         logger.trace("updatePullRequest {}", pullRequest)
         checkNotNull(pullRequest.id) { "Cannot update $pullRequest without an ID" }
-        delegate.execute(
-            UpdatePullRequest(
-                UpdatePullRequest.Variables(
-                    UpdatePullRequestInput(
-                        pullRequestId = pullRequest.id,
-                        baseRefName = pullRequest.baseRefName,
-                        title = pullRequest.title,
-                        body = pullRequest.body,
+        delegate
+            .execute(
+                UpdatePullRequest(
+                    UpdatePullRequest.Variables(
+                        UpdatePullRequestInput(
+                            pullRequestId = pullRequest.id,
+                            baseRefName = pullRequest.baseRefName,
+                            title = pullRequest.title,
+                            body = pullRequest.body,
+                        ),
                     ),
                 ),
-            ),
-        )
+            )
+            .also { response ->
+                response.checkNoErrors { logger.error("Error updating PR #{}", pullRequest.number) }
+            }
     }
 
     private suspend fun fetchRepositoryId(gitHubInfo: GitHubInfo): String {
@@ -82,4 +91,15 @@ class GitHubClient(private val delegate: GraphQLClient<*>, private val gitHubInf
 
     private val repositoryId = AtomicReference<String?>(null)
     private suspend fun repositoryId() = repositoryId.get() ?: fetchRepositoryId(gitHubInfo).also(repositoryId::set)
+
+    private fun GraphQLClientResponse<*>.checkNoErrors(onError: () -> Unit = {}) {
+        val list = errors?.takeUnless { list -> list.isEmpty() } ?: return
+
+        onError()
+        for (graphQLClientError in list) {
+            logger.error(graphQLClientError.toString())
+        }
+
+        throw GitKsprException(list.first().message)
+    }
 }
